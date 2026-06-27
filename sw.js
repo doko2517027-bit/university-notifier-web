1f77b6f3a25595b24a6f318077c95e7009007b89
doko2517027-bit <kidokohei.shonaniryo2517027@gmail.com>
2026-06-27 16:23:48 +0900

    Add PWA icons

--- sw.js
+++ sw.js
@@ -2,9 +2,12 @@ self.addEventListener("push", (event) => {
 
     const data = event.data.json();
 
-    self.registration.showNotification(data.title, {
-        body: data.body,
-        icon: "icon.png"
-    });
+    event.waitUntil(
+        self.registration.showNotification(data.title, {
+            body: data.body,
+            icon: "icon-192.png",
+            badge: "icon-192.png"
+        })
+    );
 
 });
