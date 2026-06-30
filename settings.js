70ecac644e7cf57c3f48e642e755f6fdc1a1abba
doko2517027-bit <kidokohei.shonaniryo2517027@gmail.com>
2026-06-30 09:04:47 +0900

    設定項目追加

--- settings.js
+++ settings.js
@@ -1,3 +1,17 @@
+const VERSION = "2.0.0";
+
+document.getElementById("departmentText").textContent =
+    localStorage.getItem("department") || "未登録";
+
+document.getElementById("majorText").textContent =
+    localStorage.getItem("major") || "なし";
+
+document.getElementById("gradeText").textContent =
+    localStorage.getItem("grade") || "未登録";
+
+document.getElementById("versionText").textContent =
+    `Version ${VERSION}`;
+
 document
 .getElementById("back")
 .addEventListener("click",()=>{
