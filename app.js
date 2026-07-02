b5537ab72aa54293946e4711858cac9cf86e8532
doko2517027-bit <kidokohei.shonaniryo2517027@gmail.com>
2026-07-03 07:33:37 +0900

    コースニュース

--- app.js
+++ app.js
@@ -232,14 +232,14 @@ async function loadNews() {
 
 async function loadTodaySchedule() {
 
+    const department = localStorage.getItem("department");
+    const major = localStorage.getItem("major");
+    const grade = localStorage.getItem("grade");
+
     console.log("時間割読み込み開始");
     console.log("department:", department);
     console.log("major:", major);
     console.log("grade:", grade);
-
-    const department = localStorage.getItem("department");
-    const major = localStorage.getItem("major");
-    const grade = localStorage.getItem("grade");
 
     let docId = "";
 
