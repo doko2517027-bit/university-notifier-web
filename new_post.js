82234f37a922ffe9c40eef67601324e9694c44cb
doko2517027-bit <kidokohei.shonaniryo2517027@gmail.com>
2026-07-05 04:40:09 +0900

    共通化

--- new_post.js
+++ new_post.js
@@ -33,6 +33,34 @@ await initializePage([
 
 ]);
 
+async function uploadFile(file, resourceType = "image") {
+
+    const formData = new FormData();
+
+    formData.append("file", file);
+
+    formData.append("upload_preset", "caremate_upload");
+
+    const res = await fetch(
+
+        `https://api.cloudinary.com/v1_1/vpctonjf/${resourceType}/upload`,
+
+        {
+
+            method: "POST",
+
+            body: formData
+
+        }
+
+    );
+
+    const data = await res.json();
+
+    return data.secure_url;
+
+}
+
 button.onclick = async () => {
 
     const text = document
@@ -40,15 +68,48 @@ button.onclick = async () => {
         .value
         .trim();
 
-    if (!text) {
-
-        alert("内容を入力してください");
-
+    if (
+        !text &&
+        imagePicker.files.length === 0 &&
+        pdfPicker.files.length === 0
+    ) {
+        alert("投稿内容を入力してください。");
         return;
-
     }
 
     try {
+
+        let type = "text";
+
+        let imageUrl = "";
+
+        let pdfUrl = "";
+
+        let pdfName = "";
+
+        if (imagePicker.files.length > 0) {
+
+            imageUrl = await uploadFile(
+                imagePicker.files[0],
+                "image"
+            );
+
+            type = "image";
+
+        }
+
+        if (pdfPicker.files.length > 0) {
+
+            pdfUrl = await uploadFile(
+                pdfPicker.files[0],
+                "raw"
+            );
+
+            pdfName = pdfPicker.files[0].name;
+
+            type = "pdf";
+
+        }
 
         await addDoc(collection(db, "posts"), {
 
@@ -56,13 +117,13 @@ button.onclick = async () => {
 
             text,
 
-            type: "text",
+            type,
 
-            imageUrl: "",
+            imageUrl,
 
-            pdfUrl: "",
+            pdfUrl,
 
-            pdfName: "",
+            pdfName,
 
             createdAt: serverTimestamp(),
 
@@ -125,8 +186,6 @@ pdfPicker.onchange = () => {
         `📄 ${file.name}`;
 
 };
-
-
 
 document
 .getElementById("backButton")
