6e85aeb76b55517b47a880af6056d4c1d05053e2
doko2517027-bit <kidokohei.shonaniryo2517027@gmail.com>
2026-07-07 10:53:20 +0900

    現在地表示

--- common.js
+++ common.js
@@ -431,3 +431,29 @@ export async function decryptData(encryptedText) {
     return JSON.parse(decoder.decode(decrypted));
 
 }
+
+document.addEventListener("gesturestart", e => {
+    e.preventDefault();
+});
+
+document.addEventListener("gesturechange", e => {
+    e.preventDefault();
+});
+
+document.addEventListener("gestureend", e => {
+    e.preventDefault();
+});
+
+let lastTouchEnd = 0;
+
+document.addEventListener("touchend", e => {
+
+    const now = Date.now();
+
+    if (now - lastTouchEnd <= 300) {
+        e.preventDefault();
+    }
+
+    lastTouchEnd = now;
+
+}, { passive: false });
