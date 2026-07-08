7abeb874f0f414634b4f8c5235faf0a43aa7ef09
doko2517027-bit <kidokohei.shonaniryo2517027@gmail.com>
2026-07-08 23:02:35 +0900

    すけ更新

--- sw.js
+++ sw.js
@@ -1,11 +1,3 @@
-self.addEventListener("install", event => {
-    self.skipWaiting();
-});
-
-self.addEventListener("activate", event => {
-    event.waitUntil(clients.claim());
-});
-
 self.addEventListener("push", event => {
 
     const data = event.data.json();
