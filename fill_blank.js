69d755d727c77895efde6a085b95a381767a327f
doko2517027-bit <kidokohei.shonaniryo2517027@gmail.com>
2026-07-10 15:21:53 +0900

    Save AI generated exam data

--- fill_blank.js
+++ fill_blank.js
@@ -9,7 +9,12 @@ import {
 } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  // ここは他のJSと同じ設定をそのまま貼る
+    apiKey: "AIzaSyAEtS2NGZKqHFh29kmR9OjEpshbC1yvjFY",
+    authDomain: "universitynotifier-67517.firebaseapp.com",
+    projectId: "universitynotifier-67517",
+    storageBucket: "universitynotifier-67517.firebasestorage.app",
+    messagingSenderId: "908622250178",
+    appId: "1:908622250178:web:3e355fce8698fcf179bb5b"
 };
 
 const app = initializeApp(firebaseConfig);
