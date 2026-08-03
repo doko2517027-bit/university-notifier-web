4191791605c226c46d343524d51d2d1189189255
doko2517027-bit <kidokohei.shonaniryo2517027@gmail.com>
2026-08-03 15:54:36 +0900

    手動問題作成

--- common.js
+++ common.js
@@ -1423,7 +1423,7 @@ export async function requestNotificationPermission(){
 
         const registration =
             await navigator.serviceWorker.register(
-                "/university-notifier-web/firebase-messaging-sw.js"
+                "/firebase-messaging-sw.js"
             );
 
 
