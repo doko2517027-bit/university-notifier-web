f46825b722b933f70e16ad94b58884e69a0787b0
doko2517027-bit <kidokohei.shonaniryo2517027@gmail.com>
2026-08-03 14:43:25 +0900

    手動問題作成

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -19,3 +19,30 @@ firebase.initializeApp({
 
 const messaging =
     firebase.messaging();
+
+
+messaging.onBackgroundMessage(
+    payload => {
+
+        console.log(
+            "バックグラウンド通知受信:",
+            payload
+        );
+
+
+        self.registration.showNotification(
+            payload.notification.title,
+            {
+                body:
+                    payload.notification.body,
+
+                icon:
+                    "/icon-192.png",
+
+                badge:
+                    "/icon-192.png"
+            }
+        );
+
+    }
+);
